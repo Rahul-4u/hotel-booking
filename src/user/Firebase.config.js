4fb74a9447a0ccece6eb2a06698bf738ad34bcfd
Rahul <rahulwp24@gmail.com>
2024-12-26 23:04:31 +0600

    Add .gitignore to exclude sensitive files and dependencies

--- src/user/Firebase.config.js
+++ src/user/Firebase.config.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKJgkebqqlluIl_uCxQoxWbYKjeF5yDns",
-  authDomain: "b10-a11-cb71f.firebaseapp.com",
-  projectId: "b10-a11-cb71f",
-  storageBucket: "b10-a11-cb71f.firebasestorage.app",
-  messagingSenderId: "371188420579",
-  appId: "1:371188420579:web:043ab68889542a94495ad6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
